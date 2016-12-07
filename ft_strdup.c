a4913b349dac3320e40df712991ba4e004fc7021
Romain ROUSSEL <rorousse@e1r9p16.42.fr>
2016-12-07 16:44:50 +0100

    Final version

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rorousse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/27 20:10:48 by rorousse          #+#    #+#             */
+/*   Updated: 2015/12/14 07:57:02 by rorousse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(char *s)
+{
+	char	*new;
+	int		i;
+
+	i = 0;
+	new = (char*)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (new == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		new[i] = s[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
